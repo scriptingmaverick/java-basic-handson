71dd8b18a60bcd73b4d0e3878c021e971b731282
scriptingmaverick <khasshaik12@gmail.com>
2026-04-27 15:36:03 +0530

    Implements StringUtility class using charAt() methos

--- StringUtils.java
+++ StringUtils.java
@@ -1,10 +1,9 @@
 // StringUtils
 class StringUtils{
   static boolean containsChar(String word,char letter){
-    char[] text = word.toCharArray();
-    
-    for(int i=0;i < text.length;i++){
-      if(text[i] == letter) return true;
+
+    for(int i=0;i < word.length();i++){
+      if(word.charAt(i) == letter) return true;
     }
 
     return false;
@@ -12,21 +11,19 @@ class StringUtils{
   
   static String reverse(String word){
     String result = "";
-    char[] text = word.toCharArray();  
     
-    for(int i=text.length-1; i >= 0;i--){
-      result += text[i];
+    for(int i = word.length() - 1;i >= 0;i--){
+      result += word.charAt(i);
     }
 
     return result;
   }
   
   static String replaceAll(String word,char verifier, char resultant){
-    String result = "";
-    char[] text = word.toCharArray();  
+    String result = ""; 
     
-    for(int i =0;i < text.length;i++){
-      result += (text[i] == verifier ? resultant : text[i]);
+    for(int i = 0;i < word.length();i++){
+      result += (word.charAt(i) == verifier ? resultant : word.charAt(i));
     }
 
     return result;
